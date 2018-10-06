136fdd43066276459857c9879702e5f5d3858cbc
van15h <vansolo@posteo.net>
2018-10-06 19:16:15 +0200

    added onload sample mustermann

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+function musterData() {
+  var inputName = document.getElementById("inputName");
+  inputName.value = "mustermann";
+  document.getElementById("output").textContent = "mu5termann mu5t3rmann mu5t3rm4nn must3rmann must3rm4nn musterm4nn mu5term4nn";
+}
+
 
 
 function generate(){
