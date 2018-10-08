35013df6872354a374254fcad92947f242ca4911
van15h <vansolo@posteo.net>
2018-10-08 10:00:05 +0200

    code runners to test arrays. everything is very strange here...

--- scripts/main.js
+++ scripts/main.js
@@ -44,42 +44,56 @@ function randomize(nickname){
   var newNickname = "";
   // how many chars to swap is there in nickname
   var numberOfSwaps = 0;
-  var swapCounter = 0;
   for (let i = 0; i < nickname.length; i++) {
     if(charMap.has(nickname[i])){
       numberOfSwaps++;
     }
   }
-  console.log("number of swaps: " + numberOfSwaps);
+  // array with result combinations
   var permArr = [];
-  permArr.push(new Array());
-  // console.log("size of array in each row: " + permArr.length);
+  // array from nickname string
   var nicknameArr = Array.from(nickname);
   // loop through the nickname trying to find chars to replace
   for (let i = 0; i < nickname.length; i++) {
-
     var swapChar = nicknameArr[i];
     // check if the map has this key
     if(charMap.has(nickname[i])){
+      // // test sample for code runner
+      // let a = ['k', 'v'];
+      // let b = [];
+      // var swapChar = "a";
+      // let swapCounter = 1;
+      // for (let j = 0; j < swapCounter; j++) {
+      //   b.push(a[j]);
+      //   b.push(a[j]);
+      // }
       // swapCounter++;
-      // var tempArr = new Array((permArr.length)*2);
-      var tempArr = [];
-      var index = 0;
-      for (let k = 0; k < 2; k++) {
-        tempArr.push(permArr[index]);
-      }
-      console.log(tempArr[0]);
-      console.log(tempArr[1]);
-      tempArr[0].push(charMap.get(swapChar));
-      permArr = tempArr;
+      // console.log(b);
+      // b[0].push(swapChar);
+      // b[1].push(3);
+      // console.log(b);
+      // let c = [];
+      // for (let j = 0; j < b.length; j++) {
+      //   c.push(b[j]);
+      //   c.push(b[j]);
+      // }
+      // console.log(c);
+      // console.log(c[0]);
+      // // for (let j = 0; j < c.length; j+=2) {
+      //   c[4].splice(2,1, swapChar);
+
+      // // }
+      // console.log(c);
     } else {
-      for (let j = 0; j < permArr.length; j++) {
-        permArr[j].push(swapChar);
-        // console.log("size of perm arr:" + permArr.length);
-        // console.log("first char: " + permArr[j][i]);
-      }
+      // // test sample for code runner
+      // let a = [[1,2],[3,4]];
+      // for (let j = 0; j < a.length; j++) {
+      //   a[j].push(5);
+      // }
+      // console.log(a);
     }
   }
+  // concat the result string
   for (let i = 0; i < permArr.length; i++) {
     for (let j = 0; j < nickname.length; j++) {
       newNickname += permArr[i][j];
