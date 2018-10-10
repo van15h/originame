392d41540c0861b6afb38ae1418b53019aa497e2
van15h <vansolo@posteo.net>
2018-10-10 22:19:16 +0200

    generating algorithm done

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-// TODO add intervals for output and sample
 // TODO cookies to not to show onload sample one more time
 
 // array with all replace combinations to convert to char map
@@ -14,18 +13,18 @@ var charArray = [
 ];
 var charMap = new Map(charArray);
 
-
 function musterData(inputName) {
-  // var outputElement = document.getElementById("output");
-  // var inputName = document.getElementById("inputName");
-  // inputName.value = "mustermann";
-  // setTimeout(function(){outputElement.textContent += "mu5termann" + " " }, 500);
-  // setTimeout(function(){outputElement.textContent += "mu5t3rmann" + " " }, 1000);
-  // setTimeout(function(){outputElement.textContent += "mu5t3rm4nn" + " "}, 1500);
-  // setTimeout(function(){outputElement.textContent += "must3rmann" + " "}, 2000);
-  // setTimeout(function(){outputElement.textContent += "must3rm4nn" + " "}, 2500);
-  // setTimeout(function(){outputElement.textContent += "musterm4nn" + " "}, 3000);
-  // setTimeout(function(){outputElement.textContent += "mu5term4nn" + " "}, 3500);
+  var outputElement = document.getElementById("output");
+  var inputName = document.getElementById("inputName");
+  inputName.value = "mustermann";
+  setTimeout(function(){outputElement.textContent += "mu5termann" + " " }, 300);
+  setTimeout(function(){outputElement.textContent += "mu5t3rmann" + " " }, 600);
+  setTimeout(function(){outputElement.textContent += "mu5t3rm4nn" + " "}, 900);
+  setTimeout(function(){outputElement.textContent += "must3rmann" + " "}, 1200);
+  setTimeout(function(){outputElement.textContent += "must3rm4nn" + " "}, 1500);
+  setTimeout(function(){outputElement.textContent += "musterm4nn" + " "}, 1800);
+  setTimeout(function(){outputElement.textContent += "mu5term4nn" + " "}, 2100);
+  setTimeout(function(){outputElement.textContent += "mustermann" + " "}, 2400);
 }
 
 function clearDisplay(){
@@ -42,57 +41,50 @@ function generate(){
 function randomize(nickname){
   // to save the result string
   var newNickname = "";
-  // how many chars to swap is there in nickname
-  var numberOfSwaps = 0;
+  // how many combinationsis possible in nickname
+  var combinations = 1;
   for (let i = 0; i < nickname.length; i++) {
     if(charMap.has(nickname[i])){
-      numberOfSwaps++;
+      combinations*=2;
     }
   }
-  // array with result combinations
-  var permArr = [];
+
   // array from nickname string
   var nicknameArr = Array.from(nickname);
-  // loop through the nickname trying to find chars to replace
-  for (let i = 0; i < nickname.length; i++) {
-    var swapChar = nicknameArr[i];
-    // check if the map has this key
-    if(charMap.has(nickname[i])){
-      // // test sample for code runner
-      // let a = ['k', 'v'];
-      // let b = [];
-      // var swapChar = "a";
-      // let swapCounter = 1;
-      // for (let j = 0; j < swapCounter; j++) {
-      //   b.push(a[j]);
-      //   b.push(a[j]);
-      // }
-      // swapCounter++;
-      // console.log(b);
-      // b[0].push(swapChar);
-      // b[1].push(3);
-      // console.log(b);
-      // let c = [];
-      // for (let j = 0; j < b.length; j++) {
-      //   c.push(b[j]);
-      //   c.push(b[j]);
-      // }
-      // console.log(c);
-      // console.log(c[0]);
-      // // for (let j = 0; j < c.length; j+=2) {
-      //   c[4].splice(2,1, swapChar);
+  // array with result combinations
+  var permArr = [...Array(combinations)].map(e => Array(nicknameArr.length));
 
-      // // }
-      // console.log(c);
-    } else {
-      // // test sample for code runner
-      // let a = [[1,2],[3,4]];
-      // for (let j = 0; j < a.length; j++) {
-      //   a[j].push(5);
-      // }
-      // console.log(a);
+  // fill the result array with original nickname chars
+  for (let i = 0; i < nicknameArr.length; i++) {
+    for (let j = 0; j < combinations; j++) {
+      permArr[j][i] = nicknameArr[i];
     }
   }
+
+  // start number of repeated replacer char in the column
+  var x = combinations/2;
+  // start number of x recurrences
+  var y = 1;
+  // loop through the nickname trying to find chars to replace
+  for (let i = 0; i < nicknameArr.length; i++) {
+    if(charMap.has(nicknameArr[i])){
+      // index of the row to replace char
+      let index = 0;
+      for (let k = 0; k < y; k++) {
+        for (let l=0; l < x; l++) {
+          permArr[index][i] = charMap.get(nicknameArr[i]);
+          index++;
+        }
+        // jump to the next replacement
+        index+=x;
+      }
+      // reduction of the size of replacer
+      x/=2;
+      // increase of the reccurence
+      y*=2;
+    }
+  }
+
   // concat the result string
   for (let i = 0; i < permArr.length; i++) {
     for (let j = 0; j < nickname.length; j++) {
